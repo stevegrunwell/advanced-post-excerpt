565bf387492f4a4d0b70deff9f1fb4b71c44e0c2
Steve Grunwell <stevegrunwell@gmail.com>
2016-02-21 23:10:40 -0500

    Add unit tests around the ape_post_types filter

--- tests/PHPUnit/AdvancedPostExcerptTest.php
+++ tests/PHPUnit/AdvancedPostExcerptTest.php
@@ -55,6 +55,51 @@ class AdvancedPostExcerptTest extends TestCase {
 		ape_replace_postexcerpt_meta_box();
 	}
 
+	public function test_ape_replace_postexcerpt_meta_box_uses_filter() {
+		M::wpFunction( 'get_post_types', array(
+			'times'  => 1,
+			'args'   => array( null, 'names' ),
+			'return' => array( 'post', 'page' ),
+		) );
+
+		M::wpFunction( 'post_type_supports', array(
+			'times'  => 1,
+			'args'   => array( 'post', 'excerpt' ),
+			'return' => true,
+		) );
+
+		M::wpFunction( 'post_type_supports', array(
+			'times'  => 1,
+			'args'   => array( 'page', 'excerpt' ),
+			'return' => true,
+		) );
+
+		M::wpFunction( 'remove_meta_box', array(
+			'times'  => 1,
+			'args'   => array( 'postexcerpt', array( 'post' ), 'normal' ),
+		) );
+
+		M::wpFunction( 'add_meta_box', array(
+			'times'  => 1,
+			'args'   => array(
+				'postexcerpt',
+				'Excerpt',
+				'ape_post_excerpt_meta_box',
+				array( 'post' ),
+				'normal',
+				'high',
+			),
+		) );
+
+		M::wpPassthruFunction( '__', array(
+			'times'  => 1,
+		) );
+
+		M::onFilter( 'ape_post_types' )->with( array( 'post', 'page' ) )->reply( array( 'post' ) );
+
+		ape_replace_postexcerpt_meta_box();
+	}
+
 	public function test_ape_post_excerpt_meta_box() {
 		$post = new \stdClass;
 		$post->post_excerpt = 'foo bar';
